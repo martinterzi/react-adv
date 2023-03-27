aaffe7eecddd1389c9a813b93e7c413b7e564adb
martin terzi <martin.terzi@acquappessa.com.ar>
2023-03-27 16:42:44 -0300

    equipo listo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Navigation } from './routes/Navigation'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  )
+}
+
+export default App
